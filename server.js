bc37d0883f58fa8ee2feadcbe9855bebd51ef0a7
Vinicius Oliveira <vinicius.oliveira@anexsdev.com.br>
2019-09-04 14:16:32 -0300

    rota teste

--- server.js
+++ server.js
@@ -9,3 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.listen(3000, () =>{
     console.log('App rodando na porta 3000');
 });
+
+app.get('/', (req, res) => {
+    return res.send('OI');
+});
